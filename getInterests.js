95ae9c2d1537a035acab95706fb6fbaccd39d0c7
RyanKarakoc <kochy@hotmail.co.uk>
2023-03-09 16:58:59 +0000

    commited getInterests changes

--- getInterests.js
+++ getInterests.js
@@ -1,52 +1,30 @@
-const https = require('node:https');
-const fs = require('fs');
-
-const options = {
-
-    hostname: 'nc-leaks.herokuapp.com',
-    path: '/api/confidential',
-    method: 'GET',
-
-};
-
+const https = require("node:https");
+const fs = require("fs");
 
 function getInterests() {
+  fs.readFile("./northcoders.json", (err, northcoders) => {
+    if (err) throw err;
 
-
-    // const readNorthcoders = 
-    fs.readFile('./northcoders.json', (err, northcoders) => {
-        
-        if (err) throw err;
-
-        const northString = northcoders.toString();
-        const northJson = JSON.parse(northString);
-        const northNamesArr = northJson["people"].map((item) => {
-
-            return item["username"]
-            
-        });
-    
-    
-
-
-        
-        // console.log(northNamesArr);
-
-
-       
-     
-        // northcoders.on('data', (data) => {
-        
-        //     console.log(data);
-
-
-        // });
-
-
+    const northString = northcoders.toString();
+    const northJson = JSON.parse(northString);
+    const northNamesArr = northJson["people"].map((item) => {
+      return item["username"];
     });
 
+    for (let i = 0; i < northNamesArr.length; i++) {
+      let options = {
+        hostname: "nc-leaks.herokuapp.com",
+        path: `api/people/${northNamesArr[i]}/interests`,
+        method: "GET",
+      };
 
-};
+      const request = https.request(options, (personObj) => {
+        console.log(personObj);
+      });
 
+      request.end();
+    }
+  });
+}
 
 getInterests();
